cb73a7ee2c14a59387c7687ee858c0eac29fb0db
Orion Anderson <orion@mechapower.com>
2021-07-25 10:44:52 -0400

    Formatting

--- src/report.js
+++ src/report.js
@@ -1,35 +1,22 @@
-var ipAddr = document
-    .getElementById("ip")
-    .innerHTML
+var ipAddr = document.getElementById("ip").innerHTML
+
 if (ipAddr.includes(":")) {
-    document
-        .getElementById("ip-report")
-        .classList
-        .add("border-blue")
-    document
-        .getElementById("ip-report-title")
-        .classList
-        .add("bg-blue")
-    document
-        .getElementById("emoji")
+    document.getElementById("ip-report")
+        .classList.add("border-blue")
+    document.getElementById("ip-report-title")
+        .classList.add("bg-blue")
+    document.getElementById("emoji")
         .innerHTML = "😸"
-    document
-        .getElementById("message")
+    document.getElementById("message")
         .innerHTML = "You are using IPv6 to connect to this server!"
     confetti.start(1200, 50, 150)
 } else {
-    document
-        .getElementById("ip-report")
-        .classList
-        .add("border-pink")
-    document
-        .getElementById("ip-report-title")
-        .classList
-        .add("bg-pink")
-    document
-        .getElementById("emoji")
+    document.getElementById("ip-report")
+        .classList.add("border-pink")
+    document.getElementById("ip-report-title")
+        .classList.add("bg-pink")
+    document.getElementById("emoji")
         .innerHTML = "😿"
-    document
-        .getElementById("message")
+    document.getElementById("message")
         .innerHTML = "You are using IPv4 to connect to this server."
 };
