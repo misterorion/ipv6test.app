55e0deb678ec1692edeb4cf39ab3670f0ed0b4a1
Orion Anderson <orion@mechapower.com>
2022-10-29 15:13:31 -0400

    Use new syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
 module.exports = {
-  plugins: [],
-  purge: {
-    enabled: true,
-    mode: 'all',
-    content: [
-      './srv/index.html',
-    ],
-    options: {
-      safelist: [
-        'bg-pink',
-        'border-pink',
-        'bg-purple',
-        'border-purple',
-        'bg-yellow',
-        'border-yellow',
-        'text-black',
-        'text-white'
-      ]
-    }
-  }
+  content: [
+    './srv/index.html',
+  ],
+  safelist: [
+    'bg-pink',
+    'border-pink',
+    'bg-purple',
+    'border-purple',
+    'bg-yellow',
+    'border-yellow',
+    'text-black',
+    'text-white'
+  ]
 }
