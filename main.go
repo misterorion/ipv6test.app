42f99985bb6197f892ba1f22cbc0d3c9c9311a67
Orion Anderson <orion@mechapower.com>
2024-12-04 17:56:03 +0000

    Add some logging

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"os"
 	"text/template"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rs/zerolog"
 )
 
-var t *template.Template
-var err error
+var (
+	err error
+	log zerolog.Logger
+	t   *template.Template
+)
 
 func init() {
+	log = zerolog.New(os.Stdout).With().Logger()
 	t, err = template.ParseFiles("index.tmpl")
 	if err != nil {
 		panic(err)
@@ -31,13 +36,18 @@ func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLR
 		time.Now().Format("Jan 2, 2006 15:04:05 MST"),
 	}
 
+	log.Info().
+		Str("true-client-ip", data.Ip).
+		Str("user-agent", request.Headers["user-agent"])
+
 	var buf bytes.Buffer
 
 	err = t.Execute(&buf, data)
 	if err != nil {
+		log.Error().Err(err).Msg("error executing template")
 		return events.LambdaFunctionURLResponse{
 			StatusCode: 500,
-			Body:       fmt.Sprintf("error executing template: %v", err.Error()),
+			Body:       "Internal server error",
 		}, err
 	}
 
